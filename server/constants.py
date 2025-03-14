147bd4e5e415012f89f7c19020f01e18dce78b75
OmarZOS <osjfvhng16@gmail.com>
2025-03-14 15:49:38 +0100

    fixed connectivity

--- server/constants.py
+++ server/constants.py
@@ -13,7 +13,8 @@ SQL_HOST = os.getenv("SQL_HOST","172.18.0.2")
 SQL_USER = os.getenv("SQL_USER","root")
 SQL_PASSWORD = os.getenv("SQL_PASSWORD","dev_password")
 SQL_DATABASE = os.getenv("SQL_DATABASE","gluttex")
-DB_URI = f"{SQL_SCHEMA}://{SQL_USER}:{SQL_PASSWORD}@{SQL_HOST}/{SQL_DATABASE}"
+SQL_PORT= os.getenv("SQL_PORT","3306")
+DB_URI = f"{SQL_SCHEMA}://{SQL_USER}:{SQL_PASSWORD}@{SQL_HOST}:{SQL_PORT}/{SQL_DATABASE}"
 
 
 
