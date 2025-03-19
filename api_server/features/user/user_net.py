0e3136f3259d86e681c7b7e9ba4147794c1a56f2
OmarZOS <osjfvhng16@gmail.com>
2025-03-19 22:27:34 +0100

    fixed sqlite db volumes

--- api_server/features/user/user_net.py
+++ api_server/features/user/user_net.py
@@ -14,7 +14,7 @@ async def create_user(user_data: dict):
     url = f"https://{AUTH_SERVER_NAME}:{AUTH_PORT}{AUTH_REGISTRATION_ENDPOINT}"
     
     try:
-        response = await send_post_request(url, json_data=user_data)
+        response = await send_post_request(url, payload_data=user_data)
         response.raise_for_status()  # Raises an error for 4xx/5xx responses
         return response.json()
     except Exception as e:
@@ -51,7 +51,7 @@ async def update_user_password(existing_user: AppUser_API, token: str):
     url = f"https://{AUTH_SERVER_NAME}:{AUTH_PORT}{AUTH_CHANGE_ENDPOINT}"
 
     try:
-        response = await send_put_request(url, json_data=user_update, flags=headers)
+        response = await send_put_request(url, payload_data=user_update, flags=headers)
         response.raise_for_status()
 
         # Parse response and extract new password hash
