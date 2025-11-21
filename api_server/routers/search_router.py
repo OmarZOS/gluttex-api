b2a26d0ab1a09506c12d458c17ed6dcbf6244be3
OmarZOS <osjfvhng16@gmail.com>
2025-11-21 21:01:04 +0100

    added people search, and fixed staff fetching

--- api_server/routers/search_router.py
+++ api_server/routers/search_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from features.business.supplier.supplier_search import search_supplier_by_location
-from core.models import Location, Product, ProductProvider, ProviderDetails, Recipe
+from core.models import AppUser, Location, Person, PersonDetails, Product, ProductProvider, ProviderDetails, Recipe
 from storage.storage_broker import search_records
 
 search_router = APIRouter()
@@ -11,7 +11,13 @@ def search_for_product(token:str,offset:int,limit:int):
     """
     Search products by token.
     """
-    return search_records( Product, search_query=token,search_fields=[Product.product_brand,Product.product_name,Product.product_description],offset=offset,limit=limit )
+    return search_records( Product
+                          , search_query=token
+                          ,search_fields=[Product.product_brand
+                            ,Product.product_name
+                            ,Product.product_description]
+                            ,offset=offset
+                            ,limit=limit )
 
 
 @search_router.get("/search/recipe/{token}/{offset}/{limit}")
@@ -19,7 +25,29 @@ def search_for_recipe(token:str,offset:int,limit:int):
     """
     Search recipes by token.
     """
-    return search_records(Recipe, search_query= token,search_fields= [Recipe.recipe_name,Recipe.recipe_description,Recipe.recipe_instructions],offset= offset,limit= limit)
+    return search_records(Recipe
+                          , search_query= token
+                          ,search_fields= [Recipe.recipe_name
+                                           ,Recipe.recipe_description
+                                           ,Recipe.recipe_instructions]
+                          ,offset= offset
+                          ,limit= limit)
+
+@search_router.get("/search/personnel/{token}/{offset}/{limit}")
+def search_for_user(token:str,offset:int,limit:int):
+    """
+    Search employees by token.
+    """
+    return search_records(AppUser
+                          , search_query= token
+                          ,search_fields= ['app_user_person.person_details.person_first_name'
+                                           ,'app_user_person.person_details.person_last_name'
+                                           ,'app_user_person.person_details.person_nationality'
+                                           ,'app_user_name']
+                                           ,eager_load_depth=[{AppUser.app_user_person:{Person.person_details:[PersonDetails]}}]
+                                           ,offset= offset
+                                           ,limit= limit)
+
 
 @search_router.get("/search/supplier/{token}/{offset}/{limit}")
 def search_supplier(token:str,offset:int,limit:int):
@@ -27,12 +55,11 @@ def search_supplier(token:str,offset:int,limit:int):
     Search supplier by token.
     """
     return search_records(ProviderDetails
-                          ,[
+                            ,[
                               ProviderDetails.product_provider
                               ]
-                          ,
-                          search_fields=[ProviderDetails.provider_name
-                            ,ProviderDetails.provider_contact_info
+                            ,search_fields=[ProviderDetails.provider_name
+                                ,ProviderDetails.provider_contact_info
                             # ,ProductProvider.product_provider_details_id
                             ]
                             ,search_query=token
